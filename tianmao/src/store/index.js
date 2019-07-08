26eba2faad48ebbf1e775f19cb1fa8b6cfba23d4
quminghan <1654371764@qq.com>
2019-07-08 17:29:46 +0800

    qmh

--- tianmao/src/store/index.js
+++ tianmao/src/store/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue"
 import Vuex from "vuex";
 import createLogger from "vuex/dist/logger"
+
 import home from "./modules/home.js";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
